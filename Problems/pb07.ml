133422ae5e1a14e9230aa739ce5fa562564e0d33
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2014-05-07 16:46:16 +0200

    Flatten a nested list structure

---
+++ Problems/pb07.ml
@@ -0,0 +1,8 @@
+type 'a node =
+	| One of 'a
+	| Many of 'a node list;;
+
+let rec flatten ln = match ln with
+	| [] -> []
+	| One x::q -> x::flatten q
+	| Many x::q -> List.append (flatten x) (flatten q);;
